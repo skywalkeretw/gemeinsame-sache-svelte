c3d09dbb1ca4b8e08d464be040fcb8ed92f99e26
Luke Roy <lukeroy@outlook.com>
2024-01-24 15:05:37 +0100

    create a language store to have a global language

---
+++ src/routes/language-store.js
@@ -0,0 +1,10 @@
+import { writable } from "svelte/store";
+
+const languageStore = writable("en") 
+languageStore.toggleLanguage = () => {
+    languageStore.update( currentLanguage => {
+        return currentLanguage === "en" ? "de" : "en"
+    });
+}
+
+export default languageStore
